c31cd5d334f30dee8359199b01ca9407e7fcbcfc
Shawn Reuter <reute078@morris.umn.edu>
2020-02-25 14:08:40 -0600

    Add TodoController.java  file to server
    Refactored instances of user to todo

---
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -0,0 +1,141 @@
+package umm3601.todo;
+
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.regex;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Sorts;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+import org.mongojack.JacksonCodecRegistry;
+
+import io.javalin.http.BadRequestResponse;
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+
+/**
+ * Controller that manages requests for info about todos.
+ */
+public class TodoController {
+
+  static String bodyRegex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+
+  JacksonCodecRegistry jacksonCodecRegistry = JacksonCodecRegistry.withDefaultObjectMapper();
+
+  private final MongoCollection<Todo> todoCollection;
+
+  /**
+   * Construct a controller for todos.
+   *
+   * @param database the database containing todo data
+   */
+  public TodoController(MongoDatabase database) {
+    jacksonCodecRegistry.addCodecForClass(Todo.class);
+    todoCollection = database.getCollection("todos").withDocumentClass(Todo.class)
+        .withCodecRegistry(jacksonCodecRegistry);
+  }
+
+  /**
+   * Get the single todo specified by the `id` parameter in the request.
+   *
+   * @param ctx a Javalin HTTP context
+   */
+  public void getTodo(Context ctx) {
+    String id = ctx.pathParam("id");
+    Todo todo;
+
+    try {
+      todo = todoCollection.find(eq("_id", new ObjectId(id))).first();
+    } catch(IllegalArgumentException e) {
+      throw new BadRequestResponse("The requested todo id wasn't a legal Mongo Object ID.");
+    }
+    if (todo == null) {
+      throw new NotFoundResponse("The requested todo was not found");
+    } else {
+      ctx.json(todo);
+    }
+  }
+
+  /**
+   * Delete the todo specified by the `id` parameter in the request.
+   *
+   * @param ctx a Javalin HTTP context
+   */
+  public void deleteTodo(Context ctx) {
+    String id = ctx.pathParam("id");
+    todoCollection.deleteOne(eq("_id", new ObjectId(id)));
+  }
+
+  /**
+   * Get a JSON response with a list of all the todos.
+   *
+   * @param ctx a Javalin HTTP context
+   */
+  public void getTodos(Context ctx) {
+
+    List<Bson> filters = new ArrayList<Bson>(); // start with a blank document
+
+    if (ctx.queryParamMap().containsKey("status")) {
+        boolean targetStatus = ctx.queryParam("status", Boolean.class).get();
+        filters.add(eq("status", targetStatus));
+    }
+
+    if (ctx.queryParamMap().containsKey("category")) {
+      filters.add(regex("category", ctx.queryParam("category"), "i"));
+    }
+
+    String sortBy = ctx.queryParam("sortby", "owner"); //Sort by sort query param, default is name
+    String sortOrder = ctx.queryParam("sortorder", "asc");
+
+    ctx.json(todoCollection.find(filters.isEmpty() ? new Document() : and(filters))
+      .sort(sortOrder.equals("desc") ?  Sorts.descending(sortBy) : Sorts.ascending(sortBy))
+      .into(new ArrayList<>()));
+  }
+
+  /**
+   * Get a JSON response with a list of all the todos.
+   *
+   * @param ctx a Javalin HTTP context
+   */
+  public void addNewTodo(Context ctx) {
+    Todo newTodo = ctx.bodyValidator(Todo.class)
+      .check((tdo) -> tdo.owner != null && tdo.owner.length() > 0) //Verify that the todo has a owner that is not blank
+      .check((tdo) -> tdo.body.matches(bodyRegex)) // Verify that the provided body is valid
+      .check((tdo) -> tdo.status )
+      .check((tdo) -> tdo.category != null && tdo.category.length() > 0) // Verify that the todo has a category that is not blank
+      .get();
+
+
+    todoCollection.insertOne(newTodo);
+    ctx.status(201);
+    ctx.json(ImmutableMap.of("id", newTodo._id));
+  }
+
+  /**
+   * Utility function to generate the md5 hash for a given string
+   *
+   * @param str the string to generate a md5 for
+   */
+  public String md5(String str) throws NoSuchAlgorithmException {
+    MessageDigest md = MessageDigest.getInstance("MD5");
+    byte[] hashInBytes = md.digest(str.toLowerCase().getBytes(StandardCharsets.UTF_8));
+
+    String result = "";
+    for (byte b : hashInBytes) {
+      result += String.format("%02x", b);
+    }
+    return result;
+  }
+}
